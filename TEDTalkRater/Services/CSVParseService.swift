a42ce820df5b817a4d876eecec4769b4fac02c70
bmassengill <beverly.massengill@metova.com>
2018-08-26 15:33:35 -0500

    Load all csv data in batches

--- TEDTalkRater/Services/CSVParseService.swift
+++ TEDTalkRater/Services/CSVParseService.swift
@@ -19,15 +19,16 @@ class CSVParseService {
     
     // MARK: - Data Load Methods
     
-    func loadCSVData(completion: @escaping (([TEDTalk]) -> Void)) {
+    func loadCSVData(updateHandler: @escaping (([TEDTalk]) -> Void)) {
         
         DispatchQueue.global(qos: .background).async {
             
             var tedTalks:[TEDTalk] = []
             
+            // Ensure the completion is called even if something goes wrong
             defer {
                 DispatchQueue.main.async {
-                    completion(tedTalks)
+                    updateHandler(tedTalks)
                 }
             }
             
@@ -41,12 +42,10 @@ class CSVParseService {
             
                 let csv = try CSVReader(stream: stream, hasHeaderRow: true)
                 
-                let headerRow = csv.headerRow!
-                print("\(headerRow)")
+                print("Starting CSV parse.")
+                var counter = 0
                 
-//                while csv.next() != nil {
-                for _ in 0...100 {
-                    csv.next()
+                while csv.next() != nil {
                     
                     guard let title = csv["title"],
                         let description = csv["description"],
@@ -55,16 +54,27 @@ class CSVParseService {
                         return
                     }
                     
+                    counter += 1
+                    
                     // These are being inserted into a nil context to prevent them from saving to CoreData until a rating is added by the user
                     let talk = TEDTalk(title: title, description: description, presenter: speaker, rating: 0)
                     tedTalks.append(talk)
+                    
+                    // Update the UI in batches
+                    if counter % 200 == 0 {
+                        
+                        DispatchQueue.main.async {
+                            updateHandler(tedTalks)
+                        }
+                    }
                 }
+                
+                print("CSV load complete")
             }
             catch {
                 
                 print("Exception during CSV parse")
             }
-            
         }
     }
 }
